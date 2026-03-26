720afbfb073b815bbb63627209e95a5f31f97e85
Carlos <caerlosfreefire@gmail.com>
2026-03-25 23:46:13 -0300

    ajustes pagina inicial

--- myapp.py
+++ myapp.py
@@ -9,16 +9,16 @@ from viwer import *
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
 
-#templates/ → todos os HTML
+#templates/ = todos os HTML
 
-#static/ → CSS, JS, imagens
+#static/ = CSS, JS, imagens
 
-#viwer.py → define as rotas com @app.route(...)
+#viwer.py = define as rotas com @app.route(...)
 
 #git add .
 #git commit -m "Descrição da alteração"
 #git push
 
 
-#doces, açai,lasanha, melzinho,filmes coreanos, nao muito meloso, musica internacional e um pouca de cada  menos forro e sertanejo
+
         
